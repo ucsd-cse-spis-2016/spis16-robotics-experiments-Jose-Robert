64d56d86c27435f1ffa408cbabe2fbddf8a4aeca
Diba Mirza <dimirza@eng.ucsd.edu>
2016-08-31 23:15:31 -0700

    camera runtime error

--- maze.py
+++ maze.py
@@ -8,7 +8,10 @@ import RPi.GPIO as GPIO
 import time
 from mysonar import *
 from motor import *
-from camera import *
+import cv2
+import picamera
+import picamera.array
+import time
 
 #use physical pin numbering
 GPIO.setmode(GPIO.BOARD)
@@ -21,36 +24,23 @@ def OAS(Mid_threshold, Side_Min):
     distR = getDistance(13,12)
     print distL, distM, distR
     
-#    while (distM > Mid_threshold):
- #       distL = getDistance(22,18)
-  #      distM = getDistance(8,8)
-   #     distR = getDistance(13,12)
+    while (distM > Mid_threshold):
+        distL = getDistance(22,18)
+        distM = getDistance(8,8)
+        distR = getDistance(13,12)
 
-    if (distL < Side_Min):
-        turnRight(.25)
-	# was .25 
-        print "turn right"
-    elif (distR < Side_Min):
-        turnLeft(.25)
-        print "turn left"
-    elif (distM < Mid_threshold):# delete the below block for running the while loop 
-	stopall()
-        if (distR > distL):
+        if (distL < Side_Min):
             turnRight(.25)
-            distL = getDistance(18,22)
-            distM = getDistance(8,8)
-            distR = getDistance(12,13)
-        elif (distL > distR):
-            turnLeft (.25)
-            distL = getDistance(18,22)
-            distM = getDistance(8,8)
-            distR = getDistance(12,13)
-# //////////////////////////////////////
-    else: 
-        forward(30)
-        print "forward"
-   # stopall()
-   # print "dead end" 
+	# was .25 
+            print "turn right"
+        elif (distR < Side_Min):
+            turnLeft(.25)
+            print "turn left"
+        else:
+            forward(30)
+
+    stopall()
+    return 
 
 def turnRight (TSleep):
     turn(0,20)
@@ -63,16 +53,11 @@ def turnLeft (TSleep):
     time.sleep(TSleep)
     stopall()
     #print "moving left" 
-
-#def Counter():
- #   global counter
-  #  counter += 1 
-   # print counter
-    #return counter 	
+ 	
 
 try:
     state = 0
-    M= 20
+    M= 15
     S = 15
     while (True):
         distM = getDistance(8,8)
@@ -82,13 +67,52 @@ try:
             stopall()
         if (state == 1):
             OAS(M,S)
-#            turnCount = Counter()
- #           if (turnCount == 0 or turnCount == 3 or turnCount == 4 or turnCount == 5):
-  #              turnRight(.25)
-#		print "turning right"
- #           elif (turnCount == 1 or turnCount == 2):
-  #              turnLeft(.25)
-#		print "turning left"
+            result =0
+            with picamera.PiCamera() as camera:
+                camera.resolution=(640,480)
+                camera.framerate = 32
+
+
+            with picamera.array.PiRGBArray(camera, size=(640,480)) as stream:
+
+                #capture to stream for use with opencv
+                camera.capture(stream, format='bgr')
+                image = stream.array
+                hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+                # define the range of the blue color in hsv
+                lower_Red = np.array([RedH_low, RedS_low, RedV_low])
+                upper_Red = np.array([255, 255, 255])
+                #  lower_Black = np.array([BlackH_low, BlackS_low, BlackV_low])
+                # upper_Black = np.array([BackH_high, BlackS_high, BlackV_high])
+                
+                # Threshold the hsv image to get only blue colors
+                Redmask = cv2.inRange(hsv, lower_Red, upper_Red)
+                # show the frame
+                #cv2.imshow("Frame", image)
+                #cv2.imshow("RedMask", Redmask)
+                numwhiteRedmask = cv2.countNonZero(Redmask)
+                print numwhiteRedmask
+                
+            
+                
+                if numwhiteRedmask> redthresh:
+                    result=1
+                else:
+                    result=0
+       
+            if result == 1:
+                print "red arrow found"
+                turnLeft(.25)
+            else:
+                distL = getDistance(18,22)
+                distM = getDistance(8,8)
+                distR = getDistance(12,13)
+                print "No arrow found"
+                if (distR > distL):
+                    turnRight(.25)                  
+                elif (distL > distR):
+                    turnLeft(.25)           
         else:
             state = 0
 	
---
+++ ourballdetection.py
@@ -0,0 +1,63 @@
+# This script is for continuous video capture
+# In addition it uses opencv to transform
+# the video frams to HSV and displays the video stream 
+import cv2
+import picamera
+import picamera.array 
+import time
+import numpy as np
+
+def nothing(x):
+    pass
+
+
+RedH_low, RedS_low, RedV_low = 109, 185, 82
+RedH_high, RedS_high, RedV_high = 255, 255, 255
+redthresh = 20000
+#BlackH_low, BlackS_low, BlackV_low = 85, 83, 9
+#BackH_high, BlackS_high, BlackV_high = (BlackH_low + 100), (BlackS_low + 100), (BlackV_low + 100)
+img_low = np.zeros((15,512,3),np.uint8)
+
+def findArrow():
+
+        with picamera.PiCamera() as camera:
+                camera.resolution=(640,480)
+                camera.framerate = 32
+
+
+        with picamera.array.PiRGBArray(camera, size=(640,480)) as stream:
+
+            #capture to stream for use with opencv
+            camera.capture(stream, format='bgr')
+            image = stream.array
+            hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+            # define the range of the blue color in hsv
+            lower_Red = np.array([RedH_low, RedS_low, RedV_low])
+            upper_Red = np.array([255, 255, 255])
+            #  lower_Black = np.array([BlackH_low, BlackS_low, BlackV_low])
+            # upper_Black = np.array([BackH_high, BlackS_high, BlackV_high])
+                
+            # Threshold the hsv image to get only blue colors
+            Redmask = cv2.inRange(hsv, lower_Red, upper_Red)
+            # show the frame
+            #cv2.imshow("Frame", image)
+            #cv2.imshow("RedMask", Redmask)
+            numwhiteRedmask = cv2.countNonZero(Redmask)
+            print numwhiteRedmask
+                
+            #Blackmask = cv2.inRange(hsv, lower_Black, upper_Black)
+                
+            if numwhiteRedmask> redthresh:
+                return 1
+            else:
+                return 0
+
+result = findArrow()
+if result == 1:
+    print "red arrow found"
+             
+else:       
+    print "No arrow found"
+
+
